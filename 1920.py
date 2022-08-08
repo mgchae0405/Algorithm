15ad232c471be59c8d0f181cc9a2105fe88773a1
mgchae0405 <mgchae0405@gmail.com>
2022-08-08 09:22:51 +0900

    1920 other solution

--- 1920.py
+++ 1920.py
@@ -9,3 +9,12 @@ for i in lst2:
     else:
         print(0)
 
+##############################################
+#다른 풀이
+
+# N,A = int(input()), {i for i in map(int, input().split())}
+# M = input()
+#
+# for i in list(map(int, input().split())):
+#     print(A.get(i,0))
+#
